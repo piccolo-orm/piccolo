6573d985a6333021f3c85448acde6ccd2cd3823c
Daniel Townsend <dan@dantownsend.co.uk>
2018-09-10 21:55:59 +0100

    added login method

--- aragorm/extensions/user.py
+++ aragorm/extensions/user.py
@@ -5,6 +5,8 @@ from aragorm.table import Table
 from aragorm.columns import Varchar, PrimaryKey, Boolean
 
 
+# Might change to BaseUser ... making it clearer that it needs to be
+# subclassed ...
 class User(Table):
     """
     The password needs to be hashed.
@@ -14,3 +16,29 @@ class User(Table):
     password = Varchar(length=255)
     email = Varchar(length=255)
     active = Boolean(default=False)
+
+    @classmethod
+    def hash_password(cls, password: str) -> str:
+        """
+        Hashes the password, ready for storage, and for comparing during
+        login.
+        """
+        return 'secret'
+
+    @classmethod
+    def login(cls, username: str, password: str):
+        return cls.select().where(
+            (cls.username == username) &
+            (cls.password == cls.hash_password(password))
+        ).exists()
+
+
+# Things to consider ...
+# just have some class methods around it ...
+# or some methods ...
+# One problem is ... need to define a secret key to salt the hash
+# When people subclass it ... can add the hash???
+# Need to have the ability in a table to transform values during __init__ and
+# __set__.
+# That means I don't need a create_user ...
+# BUT ... I still need a check_password ...
