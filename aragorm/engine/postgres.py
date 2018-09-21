6c9caaba1a63adfc4a2dbbd88ba192fa261919d1
Daniel Townsend <dan@dantownsend.co.uk>
2018-09-21 21:48:18 +0100

    removing connection pools for now

--- aragorm/engine/postgres.py
+++ aragorm/engine/postgres.py
@@ -1,7 +1,6 @@
 import typing as t
 
 import asyncpg
-from asyncpg.pool import Pool
 
 from .base import Engine
 
@@ -18,22 +17,8 @@ class PostgresEngine(Engine):
     def __init__(self, config: t.Dict[str, t.Any]) -> None:
         self.config = config
 
-    async def get_pool(self) -> Pool:
-        if not self.pool:
-            self.pool = await asyncpg.create_pool(
-                **self.config
-            )
-        return self.pool
-
     async def run(self, query: str):
-        pool = await self.get_pool()
-
-        connection = await pool.acquire()
-        try:
-            response = await connection.fetch(query)
-        except Exception:
-            pass
-        finally:
-            await pool.release(connection)
-
-        return response
+        conn = await asyncpg.connect(**self.config)
+        results = await conn.fetch(query)
+        await conn.close()
+        return results
