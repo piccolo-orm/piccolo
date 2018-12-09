a78ac93519ac2d66f2ab7127b10755d2e4d78cbb
Daniel Townsend <dan@dantownsend.co.uk>
2018-12-09 18:13:43 +0000

    setup.py WIP

--- setup.py
+++ setup.py
@@ -1,123 +1,42 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import io
 import os
-import sys
-from shutil import rmtree
+from setuptools import find_packages, setup
 
-from setuptools import find_packages, setup, Command
-
-# Package meta-data.
-NAME = 'piccolo'
-DESCRIPTION = 'My short description for my project.'
-URL = 'https://github.com/me/myproject'
-VERSION = None
-
-# What packages are required for this module to be executed?
-REQUIRED = [
-    # 'requests', 'maya', 'records',
-]
-
-# What packages are optional?
-EXTRAS = {
-    # 'fancy feature': ['django'],
-}
-
-# The rest you shouldn't have to touch too much :)
-# ------------------------------------------------
-# Except, perhaps the License and Trove Classifiers!
-# If you do change the License, remember to change the Trove Classifier for that!
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-# Import the README and use it as the long-description.
-# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
-try:
-    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
-except FileNotFoundError:
-    long_description = DESCRIPTION
-
-# Load the package's __version__.py module as a dictionary.
-about = {}
-if not VERSION:
-    with open(os.path.join(here, NAME, '__version__.py')) as f:
-        exec(f.read(), about)
-else:
-    about['__version__'] = VERSION
+from piccolo import __VERSION__ as VERSION
 
 
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = 'Build and publish the package.'
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
-        except OSError:
-            pass
-
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
-
-        self.status('Uploading the package to PyPI via Twine…')
-        os.system('twine upload dist/*')
-
-        self.status('Pushing git tags…')
-        os.system('git tag v{0}'.format(about['__version__']))
-        os.system('git push --tags')
-
-        sys.exit()
+directory = os.path.abspath(os.path.dirname(__file__))
 
 
-# Where the magic happens:
+with open(os.path.join(directory, 'requirements.txt')) as f:
+    contents = f.read()
+    REQUIREMENTS = [i.strip() for i in contents.strip().split('\n')]
+
+
+with open(os.path.join(directory, 'README.md')) as f:
+    LONG_DESCRIPTION = f.read()
+
+
 setup(
-    name=NAME,
-    version=about['__version__'],
-    description=DESCRIPTION,
-    long_description=long_description,
+    name='piccolo',
+    version=VERSION,
+    description='A fast, user friendly ORM which supports asyncio.',
+    long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
     author='Daniel Townsend',
     author_email='dan@dantownsend.co.uk',
     python_requires='>=3.7.0',
     url='https://github.com/telerytech/piccolo',
     packages=find_packages(exclude=('tests',)),
-    # If your package is a single module, use this instead of 'packages':
-    # py_modules=['mypackage'],
-
-    # entry_points={
-    #     'console_scripts': ['mycli=mymodule:cli'],
-    # },
-    install_requires=REQUIRED,
-    extras_require=EXTRAS,
-    include_package_data=True,
+    install_requires=REQUIREMENTS,
     license='MIT',
     classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
     ],
-    # $ setup.py publish support.
-    cmdclass={
-        'upload': UploadCommand,
-    },
 )
