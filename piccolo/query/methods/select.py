cbe864b25c2308590740d2e8f28ddff5606505a5
Daniel Townsend <daniel.townsend@worldspan.co.uk>
2018-12-18 09:39:27 +0000

    using double underscore in table alias to reduce likelihood of name clash

--- piccolo/query/methods/select.py
+++ piccolo/query/methods/select.py
@@ -48,7 +48,9 @@ class Select(
                 for index, _key in enumerate(_keys):
                     _index = index + 1
                     _key.index = _index
-                    alias = f'{tablename}{_index}'
+                    # Double underscore is just to prevent the likelihood of a
+                    # name clash.
+                    alias = f'{tablename}__{_index}'
                     _key.alias = alias
                     joins.append(
                         f' JOIN {tablename} {alias} ON {_key._name} = {alias}.id'
