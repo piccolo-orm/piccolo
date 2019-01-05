e732d8380a9481c38c1d10af11de004e60d2fbbb
Daniel Townsend <dan@dantownsend.co.uk>
2019-01-05 07:46:38 +0000

    building up call_chain by overriding __getattribute__ (wip)

--- piccolo/columns/column_types.py
+++ piccolo/columns/column_types.py
@@ -107,23 +107,63 @@ class ForeignKey(Integer):
 
     column_type = 'INTEGER'
 
-    def __init__(self, references: t.Type['table.Table'], **kwargs) -> None:
-        super().__init__(**kwargs)
-        self.references = references
+    def __getattribute__(self, name):
+        # see if it has an attribute with that name ...
+        # if it's asking for a foreign key ... return a copy of self ...
 
-        # Allow columns on the referenced table to be accessed via auto
-        # completion.
-        for column in references.Meta.columns:
+        try:
+            value = object.__getattribute__(self, name)
+        except AttributeError:
+            raise AttributeError
+
+        foreignkey_class = object.__getattribute__(self, '__class__')
+
+        if type(value) == foreignkey_class:  # i.e. a ForeignKey
+            new_column = copy.deepcopy(value)
+            new_column.call_chain = copy.copy(self.call_chain)
+            new_column.call_chain.append(self)
+
             # We have to set limits to the call chain because Table 1 can
             # reference Table 2, which references Table 1, creating an endless
             # loop. For now an arbitrary limit is set of 10 levels deep.
             # When querying a call chain more than 10 levels deep, an error
             # will be raised. Often there are more effective ways of
             # structuring a query than joining so many tables anyway.
-            if len(column.call_chain) > 10:
-                continue
+            if len(new_column.call_chain) > 10:
+                raise Exception('Call chain too long!')
 
+            for proxy_column in self.proxy_columns:
+                try:
+                    delattr(new_column, proxy_column._name)
+                except Exception:
+                    pass
+
+            for column in value.references.Meta.columns:
+                _column = copy.deepcopy(column)
+                _column.call_chain = copy.copy(new_column.call_chain)
+                _column.call_chain.append(new_column)
+                if _column._name == 'id':
+                    continue
+                setattr(new_column, _column._name, _column)
+                self.proxy_columns.append(_column)
+
+            return new_column
+        elif type(value) == Column:
+            new_column = copy.deepcopy(value)
+            new_column.call_chain = copy.copy(self.call_chain)
+            new_column.call_chain.append(self)
+            return new_column
+        else:
+            return value
+
+    def __init__(self, references: t.Type['table.Table'], **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.references = references
+        self.proxy_columns = []
+
+        # Allow columns on the referenced table to be accessed via auto
+        # completion.
+        for column in references.Meta.columns:
             _column = copy.deepcopy(column)
-            _column.call_chain.append(self)
-
             setattr(self, column._name, _column)
+            self.proxy_columns.append(_column)
