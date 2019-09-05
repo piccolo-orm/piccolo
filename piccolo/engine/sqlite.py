00e6144edbeedb576594976a758f6d157c940a31
Daniel Townsend <dan@dantownsend.co.uk>
2019-09-05 08:47:08 +0100

    not trying to run in pool with sqlite

--- piccolo/engine/sqlite.py
+++ piccolo/engine/sqlite.py
@@ -16,28 +16,21 @@ def dict_factory(cursor, row):
 
 class SQLiteEngine(Engine):
 
-    engine_type = 'sqlite'
+    engine_type = "sqlite"
 
-    def __init__(self, path: str = 'piccolo.sqlite') -> None:
+    def __init__(self, path: str = "piccolo.sqlite") -> None:
         self.path = path
 
     async def run_in_pool(
-        self,
-        query: str,
-        args: t.List[t.Any] = [],
-        query_type: str = 'generic'
+        self, query: str, args: t.List[t.Any] = [], query_type: str = "generic"
     ):
         raise NotImplementedError
 
     async def run(
-        self,
-        query: str,
-        args: t.List[t.Any] = [],
-        query_type: str = 'generic'
+        self, query: str, args: t.List[t.Any] = [], query_type: str = "generic"
     ):
         async with aiosqlite.connect(
-            self.path,
-            detect_types=sqlite3.PARSE_DECLTYPES
+            self.path, detect_types=sqlite3.PARSE_DECLTYPES
         ) as connection:
             connection.row_factory = dict_factory
             async with connection.execute(query, args) as cursor:
@@ -49,26 +42,18 @@ class SQLiteEngine(Engine):
                 # print(response)
 
                 # print(query_type)
-                if query_type == 'insert':
-                    return [{'id': cursor.lastrowid}]
+                if query_type == "insert":
+                    return [{"id": cursor.lastrowid}]
                 else:
                     return response
 
     async def run_querystring(
-        self,
-        querystring: QueryString,
-        in_pool: bool = False
+        self, querystring: QueryString, in_pool: bool = False
     ):
-        if in_pool:
-            return await self.run_in_pool(
-                *querystring.compile_string(engine_type=self.engine_type),
-                query_type=querystring.query_type
-            )
-        else:
-            return await self.run(
-                *querystring.compile_string(engine_type=self.engine_type),
-                query_type=querystring.query_type
-            )
+        return await self.run(
+            *querystring.compile_string(engine_type=self.engine_type),
+            query_type=querystring.query_type
+        )
 
     def transaction(self):
         raise NotImplementedError
