9d81b757b94120980bf885ad272ed6867e9d6b51
Daniel Townsend <dan@dantownsend.co.uk>
2020-05-14 23:58:43 +0100

    added python 3.8 classifier to setup.py

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: Implementation :: CPython",
     ],
     entry_points={"console_scripts": ["piccolo = piccolo.main:main"]},
