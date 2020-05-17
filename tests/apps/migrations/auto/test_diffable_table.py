2fe73355c199b1e0b652807455b851765d2ffba6
Daniel Townsend <dan@dantownsend.co.uk>
2020-05-17 09:12:32 +0100

    added a test for subtracting DiffableTable

--- tests/apps/migrations/auto/test_diffable_table.py
+++ tests/apps/migrations/auto/test_diffable_table.py
@@ -1,7 +1,11 @@
 
 from unittest import TestCase
 
-from piccolo.apps.migrations.auto.diffable_table import compare_dicts
+from piccolo.apps.migrations.auto.diffable_table import (
+    compare_dicts,
+    DiffableTable
+)
+from piccolo.columns import Varchar
 
 
 class TestCompareDicts(TestCase):
@@ -10,3 +14,34 @@ class TestCompareDicts(TestCase):
         dict_2 = {"a": 1, "b": 3}
         response = compare_dicts(dict_1, dict_2)
         self.assertEqual(response, {"b": 2})
+
+
+class TestDiffableTable(TestCase):
+    def test_subtract(self):
+        kwargs = {
+            'class_name': "Manager",
+            'tablename': 'manager'
+        }
+
+        name_column_1 = Varchar(unique=False)
+        name_column_1._meta.name = "name"
+        table_1 = DiffableTable(
+            **kwargs,
+            columns=[name_column_1]
+        )
+
+        name_column_2 = Varchar(unique=True)
+        name_column_2._meta.name = "name"
+        table_2 = DiffableTable(
+            **kwargs,
+            columns=[name_column_2]
+        )
+
+        delta = table_1 - table_2
+
+        self.assertEqual(
+            delta.alter_columns[0].params, {'unique': True}
+        )
+        self.assertEqual(
+            delta.alter_columns[0].old_params, {'unique': False}
+        )
