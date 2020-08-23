5810bd9845576b7f3cb8ec5b9c0451eb3bab3d66
Daniel Townsend <dan@dantownsend.co.uk>
2020-08-23 15:32:46 +0100

    added 'typed' classifier to setup.py, for PyPI

--- setup.py
+++ setup.py
@@ -48,6 +48,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: Implementation :: CPython",
+        "Typing :: Typed"
     ],
     entry_points={"console_scripts": ["piccolo = piccolo.main:main"]},
 )
