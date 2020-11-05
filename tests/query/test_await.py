00fb487341237f4b5557874e0d6dbb53d9c6226f
Daniel Townsend <dan@dantownsend.co.uk>
2020-11-05 20:22:23 +0000

    adding __await__ magic method to Query

---
+++ tests/query/test_await.py
@@ -0,0 +1,18 @@
+import asyncio
+
+from ..base import DBTestCase
+from ..example_project.tables import Band
+
+
+class TestAwait(DBTestCase):
+    def test_await(self):
+        """
+        Test awaiting a query directly - it should proxy to Query.run().
+        """
+
+        async def get_all():
+            return await Band.select()
+
+        response = asyncio.run(get_all())
+
+        self.assertIsInstance(response, list)
