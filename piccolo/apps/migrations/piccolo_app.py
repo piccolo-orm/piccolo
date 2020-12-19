266706abd5789ea6df8c44d176599cc16262303d
Daniel Townsend <dan@dantownsend.co.uk>
2020-12-19 23:01:25 +0000

    registering clean command with app

--- piccolo/apps/migrations/piccolo_app.py
+++ piccolo/apps/migrations/piccolo_app.py
@@ -1,6 +1,7 @@
 from piccolo.conf.apps import AppConfig
 from .commands.backwards import backwards
 from .commands.check import check
+from .commands.clean import clean
 from .commands.forwards import forwards
 from .commands.new import new
 
@@ -8,5 +9,5 @@ from .commands.new import new
 APP_CONFIG = AppConfig(
     app_name="migrations",
     migrations_folder_path="",
-    commands=[backwards, check, forwards, new],
+    commands=[backwards, check, clean, forwards, new],
 )
