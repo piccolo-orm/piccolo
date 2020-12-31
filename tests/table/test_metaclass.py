541abb1ad2c2831ec1a54970ca516d6ce91959e9
Daniel Townsend <dan@dantownsend.co.uk>
2020-12-31 11:47:42 +0000

    merged test_meta and test_metaclass

--- tests/table/test_metaclass.py
+++ tests/table/test_metaclass.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 
+from piccolo.table import Table
+
 from ..example_app.tables import Band
 
 
@@ -7,3 +9,17 @@ class TestMetaClass(TestCase):
     def test_tablename(self):
         self.assertEqual(Band._meta.tablename, "band")
 
+    def test_protected_table_names(self):
+        """
+        Some tablenames are forbidden because they're reserved words in the
+        database, and can potentially cause issues.
+        """
+        with self.assertRaises(ValueError):
+
+            class User(Table):
+                pass
+
+        with self.assertRaises(ValueError):
+
+            class MyUser(Table, tablename="user"):
+                pass
