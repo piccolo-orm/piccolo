699a01fcd7c9412d2f5c2c5c15725ef21a37ece0
Daniel Townsend <dan@dantownsend.co.uk>
2021-05-12 22:23:14 +0100

    added `change_permissions` command

---
+++ piccolo/apps/user/commands/change_permissions.py
@@ -0,0 +1,50 @@
+import typing as t
+
+from piccolo.apps.user.tables import BaseUser
+from piccolo.utils.warnings import colored_string, Level
+
+if t.TYPE_CHECKING:
+    from piccolo.columns import Column
+
+
+async def change_permissions(
+    username: str,
+    admin: t.Optional[bool] = None,
+    superuser: t.Optional[bool] = None,
+    active: t.Optional[bool] = None,
+):
+    """
+    Change a user's permissions.
+
+    :param username:
+        Change the permissions for this user.
+    :param admin:
+        Set `admin` for the user (true / false).
+    :param superuser:
+        Set `superuser` for the user (true / false).
+    :param active:
+        Set `active` for the user (true / false).
+
+    """
+    if not await BaseUser.exists().where(BaseUser.username == username).run():
+        print(
+            colored_string(
+                f"User {username} doesn't exist!", level=Level.medium
+            )
+        )
+        return
+
+    params: t.Dict[Column, bool] = {}
+
+    if admin is not None:
+        params[BaseUser.admin] = admin
+
+    if superuser is not None:
+        params[BaseUser.superuser] = superuser
+
+    if active is not None:
+        params[BaseUser.active] = active
+
+    await BaseUser.update(params).where(BaseUser.username == username).run()
+
+    print(f"Updated permissions for {username}")
