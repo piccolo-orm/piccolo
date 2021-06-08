9d04e2fab5312ef874a012b8d477f80a130ba0f6
Daniel Townsend <dan@dantownsend.co.uk>
2021-06-08 21:51:40 +0100

    add additional tests for frozen queries

--- tests/query/test_freeze.py
+++ tests/query/test_freeze.py
@@ -1,11 +1,10 @@
 from dataclasses import dataclass
 import timeit
 import typing as t
-from unittest.mock import patch
 
 from piccolo.query.base import Query
 
-from tests.base import DBTestCase
+from tests.base import DBTestCase, sqlite_only
 from tests.example_app.tables import Band
 
 
@@ -52,43 +51,62 @@ class TestFreeze(DBTestCase):
                 ),
                 response=[{"name": "Pythonistas"}],
             ),
-            QueryResponse(
-                query=(
-                    Band.select(Band.name)
-                    .where(Band.name == "Pythonistas")
-                    .output(as_json=True)
-                    .freeze()
-                ),
-                response='[{"name":"Pythonistas"}]',
-            ),
         ]
 
         for query_response in query_responses:
             result = query_response.query.run_sync()
             self.assertEqual(result, query_response.response)
 
-    # def test_frozen_performance(self):
-    #     """
-    #     The frozen query performance should exceed the non-frozen. If not,
-    #     there's a problem.
-    #     """
-    #     with patch.object(Band._meta.db, "run_querystring"):
-    #         pass
+    def test_output_clause(self):
+        """
+        Make sure the output clause still works correctly with frozen queries.
+        """
+        self.insert_rows()
 
-    #     iterations = 50
-    #     query = Band.select().where(Band.name == "Pythonistas").first()
-    #     query_duration = timeit.timeit(
-    #         lambda: query.run_sync(), number=iterations
-    #     )
+        result = (
+            Band.select(Band.name)
+            .where(Band.name == "Pythonistas")
+            .output(as_json=True)
+            .freeze()
+            .run_sync()
+        )
+        # Some JSON encoders have a space after the colons and some don't,
+        # so normalise them.
+        self.assertEqual(result.replace(" ", ""), '[{"name":"Pythonistas"}]')
 
-    #     # patch out the actual database call ... patching the engine is
-    #     # quite challenging ...
+    @sqlite_only
+    def test_frozen_performance(self):
+        """
+        The frozen query performance should exceed the non-frozen. If not,
+        there's a problem.
 
-    #     frozen_query = query.freeze()
-    #     frozen_query_duration = timeit.timeit(
-    #         lambda: frozen_query.run_sync(), number=iterations
-    #     )
-    #     self.assertTrue(query_duration > frozen_query_duration)
+        Only test this on SQLite, as the latency from the database itself
+        is more predictable than with Postgres, and the test runs quickly.
 
-    # def test_attribute_access(self):
-    #     pass
+        """
+        iterations = 50
+        query = Band.select().where(Band.name == "Pythonistas").first()
+        query_duration = timeit.repeat(
+            lambda: query.run_sync(), repeat=iterations, number=1
+        )
+
+        frozen_query = query.freeze()
+        frozen_query_duration = timeit.repeat(
+            lambda: frozen_query.run_sync(), repeat=iterations, number=1
+        )
+
+        # Remove the outliers before comparing
+        self.assertTrue(
+            sum(sorted(query_duration)[5:-5])
+            > sum(sorted(frozen_query_duration)[5:-5])
+        )
+
+    def test_attribute_access(self):
+        """
+        Once frozen, you shouldn't be able to call additional methods on the
+        query (for example `.where`).
+        """
+        query = Band.select().freeze()
+
+        with self.assertRaises(AttributeError):
+            query.where(Band.name == "Pythonistas")
