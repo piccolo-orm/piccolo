baf35b395becc165c6304b5841053cfdf63bdef8
Daniel Townsend <dan@dantownsend.co.uk>
2021-06-29 14:42:22 +0100

    add extra tests for inserting and updating JSON

--- tests/columns/test_json.py
+++ tests/columns/test_json.py
@@ -20,20 +20,42 @@ class MyTableDefault(Table):
     json_none = JSON(default=None, null=True)
 
 
-class TestJSON(TestCase):
+class TestJSONSave(TestCase):
     def setUp(self):
         MyTable.create_table().run_sync()
 
     def tearDown(self):
         MyTable.alter().drop_table().run_sync()
 
-    def test_json(self):
+    def test_json_string(self):
         """
         Test storing a valid JSON string.
         """
         row = MyTable(json='{"a": 1}')
         row.save().run_sync()
-        self.assertEqual(row.json, '{"a": 1}')
+
+        self.assertEqual(
+            MyTable.select(MyTable.json)
+            .first()
+            .run_sync()["json"]
+            .replace(" ", ""),
+            '{"a":1}',
+        )
+
+    def test_json_object(self):
+        """
+        Test storing a valid JSON object.
+        """
+        row = MyTable(json={"a": 1})
+        row.save().run_sync()
+
+        self.assertEqual(
+            MyTable.select(MyTable.json)
+            .first()
+            .run_sync()["json"]
+            .replace(" ", ""),
+            '{"a":1}',
+        )
 
 
 class TestJSONDefault(TestCase):
@@ -57,3 +79,72 @@ class TestJSONDefault(TestCase):
         with self.assertRaises(ValueError):
             for value in ("a", 1, ("x", "y", "z")):
                 JSON(default=value)
+
+
+class TestJSONInsert(TestCase):
+    def setUp(self):
+        MyTable.create_table().run_sync()
+
+    def tearDown(self):
+        MyTable.alter().drop_table().run_sync()
+
+    def check_response(self):
+        self.assertEqual(
+            MyTable.select(MyTable.json)
+            .first()
+            .run_sync()["json"]
+            .replace(" ", ""),
+            '{"message":"original"}',
+        )
+
+    def test_json_string(self):
+        """
+        Test inserting using a string.
+        """
+        row = MyTable(json='{"message": "original"}')
+        MyTable.insert(row).run_sync()
+        self.check_response()
+
+    def test_json_object(self):
+        """
+        Test inserting using an object.
+        """
+        row = MyTable(json={"message": "original"})
+        MyTable.insert(row).run_sync()
+
+
+class TestJSONUpdate(TestCase):
+    def setUp(self):
+        MyTable.create_table().run_sync()
+
+    def tearDown(self):
+        MyTable.alter().drop_table().run_sync()
+
+    def add_row(self):
+        row = MyTable(json={"message": "original"})
+        row.save().run_sync()
+
+    def check_response(self):
+        self.assertEqual(
+            MyTable.select(MyTable.json)
+            .first()
+            .run_sync()["json"]
+            .replace(" ", ""),
+            '{"message":"updated"}',
+        )
+
+    def test_json_update_string(self):
+        """
+        Test updating a JSON field using a string.
+        """
+        self.add_row()
+        MyTable.update({MyTable.json: '{"message": "updated"}'}).run_sync()
+        self.check_response()
+
+    def test_json_update_object(self):
+        """
+        Test updating a JSON field using an object.
+        """
+        self.add_row()
+        MyTable.update({MyTable.json: {"message": "updated"}}).run_sync()
+        self.check_response()
