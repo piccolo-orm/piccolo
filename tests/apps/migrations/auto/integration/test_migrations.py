cc7c5dfc49c095fa1f322f5a421c03f49c5f16db
Daniel Townsend <dan@dantownsend.co.uk>
2021-07-02 17:11:12 +0100

    a bunch more migration tests

--- tests/apps/migrations/auto/integration/test_migrations.py
+++ tests/apps/migrations/auto/integration/test_migrations.py
@@ -1,12 +1,15 @@
+from __future__ import annotations
 import os
 import shutil
 import tempfile
 import time
 import typing as t
 from unittest import TestCase
+import uuid
 
+from piccolo.columns.defaults.uuid import UUID4
 from piccolo.conf.apps import AppConfig
-from piccolo.columns.column_types import Integer, Varchar
+from piccolo.columns.column_types import Integer, Text, UUID, Varchar
 from piccolo.apps.migrations.commands.new import (
     _create_new_migration,
     _create_migrations_folder,
@@ -16,6 +19,17 @@ from piccolo.table import Table, create_table_class
 from piccolo.apps.migrations.tables import Migration
 from piccolo.utils.sync import run_sync
 from tests.base import postgres_only
+
+if t.TYPE_CHECKING:
+    from piccolo.columns.base import Column
+
+
+def string_default():
+    return "hello world"
+
+
+def integer_default():
+    return 1
 
 
 @postgres_only
@@ -39,8 +53,7 @@ class TestMigrations(TestCase):
 
         if os.path.exists(migrations_folder_path):
             shutil.rmtree(migrations_folder_path)
-
-        _create_migrations_folder(migrations_folder_path)
+            _create_migrations_folder(migrations_folder_path)
 
         app_config = AppConfig(
             app_name="test_app",
@@ -64,44 +77,79 @@ class TestMigrations(TestCase):
             # TODO - check the migrations ran correctly
 
     ###########################################################################
-    # Writing all of these by hand is going to be really tedious ...
 
-    def test_add_varchar_column(self):
+    def table(self, column: Column):
+        return create_table_class(
+            class_name="MyTable", class_members={"my_column": column}
+        )
+
+    def test_varchar_column(self):
         self._test_migrations(
             table_classes=[
-                create_table_class("MyTable"),
-                create_table_class(
-                    "MyTable", class_members={"name": Varchar()}
-                ),
+                self.table(column)
+                for column in [
+                    Varchar(),
+                    Varchar(length=100),
+                    Varchar(default="hello world"),
+                    Varchar(default=string_default),
+                    Varchar(null=True),
+                    Varchar(null=False),
+                    Varchar(index=True),
+                    Varchar(index=False),
+                ]
             ]
         )
 
-    def test_remove_varchar_column(self):
+    def test_text_column(self):
         self._test_migrations(
             table_classes=[
-                create_table_class(
-                    "MyTable", class_members={"name": Varchar()}
-                ),
-                create_table_class("MyTable"),
+                self.table(column)
+                for column in [
+                    Text(),
+                    Text(default="hello world"),
+                    Text(default=string_default),
+                    Text(null=True),
+                    Text(null=False),
+                    Text(index=True),
+                    Text(index=False),
+                ]
             ]
         )
 
-    def test_add_integer_column(self):
+    def test_integer_column(self):
         self._test_migrations(
             table_classes=[
-                create_table_class("MyTable"),
-                create_table_class(
-                    "MyTable", class_members={"name": Integer()}
-                ),
+                self.table(column)
+                for column in [
+                    Integer(),
+                    Integer(default=1),
+                    Integer(default=integer_default),
+                    Integer(null=True),
+                    Integer(null=False),
+                    Integer(index=True),
+                    Integer(index=False),
+                ]
             ]
         )
 
-    def test_remove_integer_column(self):
+    def test_uuid_column(self):
         self._test_migrations(
             table_classes=[
-                create_table_class(
-                    "MyTable", class_members={"name": Integer()}
-                ),
-                create_table_class("MyTable"),
+                self.table(column)
+                for column in [
+                    UUID(),
+                    # UUID(default="ecf338cd-6da7-464c-b31e-4b2e3e12f0f0"),
+                    # UUID(
+                    #     default=uuid.UUID(
+                    #         "2dfc9c47-adab-4692-b804-f692f3b0fc07"
+                    #     )
+                    # ),
+                    UUID(default=uuid.uuid4),
+                    UUID(default=UUID4),
+                    UUID(null=True),
+                    UUID(null=False),
+                    UUID(index=True),
+                    UUID(index=False),
+                ]
             ]
         )
