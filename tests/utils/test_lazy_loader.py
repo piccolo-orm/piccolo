3fb8de9ad2970fca766fc587b11e807d500e86ce
Amin Alaee <mohammadamin.alaee@gmail.com>
2021-08-04 19:45:59 +0430

    mock module instead of object

--- tests/utils/test_lazy_loader.py
+++ tests/utils/test_lazy_loader.py
@@ -1,8 +1,5 @@
 from unittest import TestCase, mock
 
-import aiosqlite
-import asyncpg  # type: ignore
-
 from piccolo.utils.lazy_loader import LazyLoader
 
 
@@ -13,7 +10,7 @@ class TestLazyLoader(TestCase):
     def test_lazy_loader_asyncpg_exception(self):
         lazy_loader = LazyLoader("asyncpg", globals(), "asyncpg.connect")
 
-        with mock.patch.object(asyncpg, "connect") as module:
+        with mock.patch("asyncpg.connect") as module:
             module.side_effect = ModuleNotFoundError()
             with self.assertRaises(ModuleNotFoundError):
                 lazy_loader._load()
@@ -21,7 +18,7 @@ class TestLazyLoader(TestCase):
     def test_lazy_loader_aiosqlite_exception(self):
         lazy_loader = LazyLoader("aiosqlite", globals(), "aiosqlite.connect")
 
-        with mock.patch.object(aiosqlite, "connect") as module:
+        with mock.patch("aiosqlite.connect") as module:
             module.side_effect = ModuleNotFoundError()
             with self.assertRaises(ModuleNotFoundError):
                 lazy_loader._load()
