10c19be3d7f03cb344718a49f1fdc467c3d2d1c2
Daniel Townsend <dan@dantownsend.co.uk>
2021-08-28 21:08:16 +0100

    allow a subset of columns to be specified

--- tests/table/instance/test_to_dict.py
+++ tests/table/instance/test_to_dict.py
@@ -13,6 +13,16 @@ class TestToDict(DBTestCase):
         dictionary = instance.to_dict()
         self.assertEqual(dictionary, {"id": 1, "name": "Guido"})
 
+    def test_filter_rows(self):
+        """
+        Make sure that `to_dict` works correctly with a subset of columns.
+        """
+        self.insert_row()
+
+        instance = Manager.objects().first().run_sync()
+        dictionary = instance.to_dict(Manager.name)
+        self.assertEqual(dictionary, {"name": "Guido"})
+
     def test_to_dict_aliases(self):
         """
         Make sure that `to_dict` works correctly with aliases.
@@ -20,5 +30,7 @@ class TestToDict(DBTestCase):
         self.insert_row()
 
         instance = Manager.objects().first().run_sync()
-        dictionary = instance.to_dict(Manager.name.as_alias("title"))
+        dictionary = instance.to_dict(
+            Manager.id, Manager.name.as_alias("title")
+        )
         self.assertEqual(dictionary, {"id": 1, "title": "Guido"})
