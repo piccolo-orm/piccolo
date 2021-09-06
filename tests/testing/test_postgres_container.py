2bae657c2b96b6a56edbf05c2e4e7a69e3be3d67
antonio-one <antonio.one@pm.me>
2021-09-06 16:46:22 +0100

    reformatting

--- tests/testing/test_postgres_container.py
+++ tests/testing/test_postgres_container.py
@@ -1,18 +1,19 @@
-from tests.postgres_container import TestPostgres, TEST_CONTAINER_IDENTIFIER
-import docker
+import docker  # type: ignore
+
+from tests.postgres_container import CONTAINER_IDENTIFIER, TestPostgres
 
 
 def test_container_status():
     test_postgres = TestPostgres()
     client = docker.client.from_env()
     container_list = client.containers.list(
-            filters={"label": f"test_container_identifier={TEST_CONTAINER_IDENTIFIER}"}
-        )
+        filters={"label": f"test_container_identifier={CONTAINER_IDENTIFIER}"}
+    )
     assert len(container_list) == 1
 
     test_postgres.tear_down()
 
     container_list = client.containers.list(
-            filters={"label": f"test_container_identifier={TEST_CONTAINER_IDENTIFIER}"}
-        )
+        filters={"label": f"test_container_identifier={CONTAINER_IDENTIFIER}"}
+    )
     assert len(container_list) == 0
