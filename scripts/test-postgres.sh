597e9f000d73e5fc94afbefe662d856a9e98261d
antonio-one <antonio.one@pm.me>
2021-09-06 16:45:00 +0100

    adding spinup and teardown

--- scripts/test-postgres.sh
+++ scripts/test-postgres.sh
@@ -5,4 +5,6 @@
 # To run a single test tests/test_foo.py::TestFoo::test_foo
 
 export PICCOLO_CONF="tests.postgres_conf"
+python -m tests.utils.setup --db_engine=postgres --action=spin_up
 python -m pytest --cov=piccolo --cov-report xml --cov-report html --cov-fail-under 85 -s $@
+python -m tests.utils.setup --db_engine=postgres --action=tear_down
