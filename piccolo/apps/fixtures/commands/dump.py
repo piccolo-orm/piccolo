1deceb72c8afc8c12c877b5b1e5556b0a1bda96d
Daniel Townsend <dan@dantownsend.co.uk>
2021-09-12 12:08:18 +0100

    prototype for fixture dumping

---
+++ piccolo/apps/fixtures/commands/dump.py
@@ -0,0 +1,61 @@
+from piccolo.apps.migrations.auto.migration_manager import sort_table_classes
+from piccolo.conf.apps import Finder
+from piccolo.utils.encoding import dump_json
+
+
+async def dump(apps: str = "all", tables: str = "all"):
+    """
+    Serialises the data from the given Piccolo apps / tables, and prints it
+    out.
+
+    :param apps:
+        For all apps, specify `all`. For specific apps, pass in a comma
+        separated list e.g. `blog,profiles,billing`. For a single app, just
+        pass in the name of that app, e.g. `blog`.
+    :param tables:
+        For all tables, specify `all`. For specific tables, pass in a comma
+        separated list e.g. `Post,Tag`. For a single app, just
+        pass in the name of that app, e.g. `Post`.
+
+    """
+    finder = Finder()
+    app_names = []
+
+    ###########################################################################
+
+    if apps == "all":
+        app_names = finder.get_sorted_app_names()
+    elif "," in apps:
+        app_names = apps.split(",")
+    else:
+        # Must be a single app name
+        app_names.append(apps)
+
+    ###########################################################################
+
+    output = {}
+
+    for app_name in app_names:
+        app_config = finder.get_app_config(app_name=app_name)
+        table_classes = app_config.table_classes
+
+        if tables != "all":
+            if "," in tables:
+                table_class_names = apps.split(",")
+            else:
+                # Must be a single table class name
+                table_class_names = [tables]
+
+            table_classes = [
+                i for i in table_classes if i.__name__ in table_class_names
+            ]
+
+        # Sort the table classes based on their ForeignKey columns
+        sorted_table_classes = sort_table_classes(table_classes)
+
+        for table_class in sorted_table_classes:
+            data = await table_class.select().run()
+            output[table_class._meta.tablename] = data
+
+    json_output = dump_json(output)
+    print(json_output)
