c86170721976976e5477e48734986d8129e9d5c4
Daniel Townsend <dan@dantownsend.co.uk>
2021-09-25 22:40:17 +0100

    split out new tables for now

--- tests/example_apps/music/tables_detailed.py
+++ tests/example_apps/music/tables_detailed.py
@@ -1,12 +1,26 @@
+# TODO - these are much better example tables than in tables.py, but many
+# tests will break if we change them. In the future migrate this file to
+# tables.py and fix the tests.
+
+from datetime import timedelta
 from enum import Enum
 
 from piccolo.columns import (
     JSON,
     JSONB,
+    UUID,
+    BigInt,
+    Boolean,
+    Bytea,
+    Date,
     ForeignKey,
     Integer,
+    Interval,
     Numeric,
+    SmallInt,
     Text,
+    Timestamp,
+    Timestamptz,
     Varchar,
 )
 from piccolo.columns.readable import Readable
@@ -18,6 +32,7 @@ from piccolo.table import Table
 
 class Manager(Table):
     name = Varchar(length=50)
+    touring = Boolean(default=False)
 
     @classmethod
     def get_readable(cls) -> Readable:
@@ -25,6 +40,8 @@ class Manager(Table):
 
 
 class Band(Table):
+    label_id = UUID()
+    date_signed = Date()
     name = Varchar(length=50)
     manager = ForeignKey(Manager, null=True)
     popularity = Integer(default=0)
@@ -44,10 +61,15 @@ class Concert(Table):
     band_2 = ForeignKey(Band)
     venue = ForeignKey(Venue)
 
+    duration = Interval(default=timedelta(weeks=5, days=3))
+    net_profit = SmallInt(default=-32768)
+
 
 class Ticket(Table):
     concert = ForeignKey(Concert)
     price = Numeric(digits=(5, 2))
+    purchase_time = Timestamp()
+    purchase_time_tz = Timestamptz()
 
 
 class Poster(Table, tags=["special"]):
@@ -55,6 +77,7 @@ class Poster(Table, tags=["special"]):
     Has tags for tests which need it.
     """
 
+    image = Bytea(default=b"\xbd\x78\xd8")
     content = Text()
 
 
@@ -76,5 +99,6 @@ class RecordingStudio(Table):
     Used for testing JSON and JSONB columns.
     """
 
-    facilities = JSON()
+    facilities = JSON(default={"amplifier": False, "microphone": True})
     facilities_b = JSONB()
+    records = BigInt(default=9223372036854775807)
