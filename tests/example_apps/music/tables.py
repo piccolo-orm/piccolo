dab4f9d641642b1dbc49bd5029400ccc6995a945
William Michael Short <36488354+wmshort@users.noreply.github.com>
2021-09-25 22:35:57 +0100

    Add defaults in automatic schema generation (#263)
    * initial prototype of defaults generation
    
    * update example tables for testing defaults generation
    
    * clean up imports
    
    * fix date default parsing
    
    * add double precision
    
    * linting
    
    * fix BigInt and SmallInt parsing
    
    * add interval parsing
    
    * linting
    
    * code clean up
    
    * code clean up
    
    * type fix
    
    * oops
    
    * clean up
    
    * Update generate.py
    
    Co-authored-by: William Michael Short <wmshort@epexegetical.ai>

--- tests/example_apps/music/tables.py
+++ tests/example_apps/music/tables.py
@@ -1,13 +1,23 @@
+from datetime import timedelta
 from enum import Enum
 
 from piccolo.columns import (
     JSON,
     JSONB,
+    UUID,
+    BigInt,
+    Boolean,
+    Bytea,
+    Date,
     ForeignKey,
     Integer,
+    Interval,
     Numeric,
+    SmallInt,
     Text,
-    Varchar,
+    Timestamp,
+    Timestamptz,
+    Varchar
 )
 from piccolo.columns.readable import Readable
 from piccolo.table import Table
@@ -18,6 +28,7 @@ from piccolo.table import Table
 
 class Manager(Table):
     name = Varchar(length=50)
+    touring = Boolean(default=False)
 
     @classmethod
     def get_readable(cls) -> Readable:
@@ -25,6 +36,8 @@ class Manager(Table):
 
 
 class Band(Table):
+    label_id = UUID()
+    date_signed = Date()
     name = Varchar(length=50)
     manager = ForeignKey(Manager, null=True)
     popularity = Integer(default=0)
@@ -44,10 +57,15 @@ class Concert(Table):
     band_2 = ForeignKey(Band)
     venue = ForeignKey(Venue)
 
+    duration = Interval(default=timedelta(weeks=5, days=3))
+    net_profit = SmallInt(default=-32768)
+
 
 class Ticket(Table):
     concert = ForeignKey(Concert)
     price = Numeric(digits=(5, 2))
+    purchase_time = Timestamp()
+    purchase_time_tz = Timestamptz()
 
 
 class Poster(Table, tags=["special"]):
@@ -55,6 +73,7 @@ class Poster(Table, tags=["special"]):
     Has tags for tests which need it.
     """
 
+    image = Bytea(default=b"\xbd\x78\xd8")
     content = Text()
 
 
@@ -76,5 +95,6 @@ class RecordingStudio(Table):
     Used for testing JSON and JSONB columns.
     """
 
-    facilities = JSON()
+    facilities = JSON(default={"amplifier": False, "microphone": True})
     facilities_b = JSONB()
+    records = BigInt(default=9223372036854775807)
