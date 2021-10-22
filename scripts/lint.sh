621bcc077c5005334c623ec8fa7bfeebf4555d02
Daniel Townsend <dan@dantownsend.co.uk>
2021-10-22 10:32:21 +0100

    make lint.sh formatters fail build

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
+set -e
 
 SOURCES="piccolo tests"
 
-isort $SOURCES
-black $SOURCES
+echo "Running isort..."
+isort --check $SOURCES
+echo "-----"
+
+echo "Running black..."
+black --check $SOURCES
+echo "-----"
+
+echo "Running flake8..."
 flake8 $SOURCES
+echo "-----"
+
+echo "Running mypy..."
 mypy $SOURCES
+echo "-----"
+
+echo "All passed!"
