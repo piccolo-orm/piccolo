f0f5e17b6284616b0ef9d53af3eb30c9269334a0
Daniel Townsend <dan@dantownsend.co.uk>
2021-11-03 06:16:16 +0000

    trying different themes

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -84,7 +84,25 @@ autodoc_typehints = "signature"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+
+html_theme = "furo"
+pygments_style = "monokai"
+pygments_dark_style = "monokai"
+
+# html_theme = "sphinx_book_theme"
+# html_theme = "sphinx_rtd_theme"
+# html_theme = "pydata_sphinx_theme"
+# html_theme = "sphinxawesome_theme"
+
+# html_theme = "sphinx_material"
+# html_sidebars = {
+#     "**": [
+#         "logo-text.html",
+#         "globaltoc.html",
+#         "localtoc.html",
+#         "searchbox.html",
+#     ]
+# }
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
