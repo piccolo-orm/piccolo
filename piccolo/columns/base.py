019cf6c4edad1e71471391d3fd8cf5607552ed47
Daniel Townsend <dan@dantownsend.co.uk>
2021-11-14 20:55:55 +0000

    added `Column._equals` method (#349)
    * added `Column._equals` method
    
    * added an extra test
    
    * add more tests

--- piccolo/columns/base.py
+++ piccolo/columns/base.py
@@ -540,6 +540,64 @@ class Column(Selectable):
     def __ge__(self, value) -> Where:
         return Where(column=self, value=value, operator=GreaterEqualThan)
 
+    def _equals(self, column: Column, including_joins: bool = False) -> bool:
+        """
+        We override ``__eq__``, in order to do queries such as:
+
+        .. code-block:: python
+
+            Band.select().where(Band.name == 'Pythonistas').run_sync()
+
+        But this means that comparisons such as this can give unexpected
+        results:
+
+        .. code-block:: python
+
+            # We would expect the answer to be `True`, but we get `Where`
+            # instead:
+            >>> MyTable.some_column == MyTable.some_column
+            <Where>
+
+        Also, column comparison is sometimes more complex than it appears. This
+        is why we have this custom method for comparing columns.
+
+        Take this example:
+
+        .. code-block:: python
+
+            Band.manager.name == Manager.name
+
+        They both refer to the ``name`` column on the ``Manager`` table, except
+        one has joins and the other doesn't.
+
+        :param including_joins:
+            If ``True``, then we check if the columns are the same, as well as
+            their joins, i.e. ``Band.manager.name`` != ``Manager.name``.
+
+        """
+        if isinstance(column, Column):
+            if (
+                self._meta.name == column._meta.name
+                and self._meta.table._meta.tablename
+                == column._meta.table._meta.tablename
+            ):
+                if including_joins:
+                    if len(column._meta.call_chain) == len(
+                        self._meta.call_chain
+                    ):
+                        return all(
+                            column_a._equals(column_b, including_joins=False)
+                            for column_a, column_b in zip(
+                                column._meta.call_chain,
+                                self._meta.call_chain,
+                            )
+                        )
+
+                else:
+                    return True
+
+        return False
+
     def __eq__(self, value) -> Where:  # type: ignore
         if value is None:
             return Where(column=self, operator=IsNull)
--- tests/columns/test_base.py
+++ tests/columns/test_base.py
@@ -4,6 +4,7 @@ from unittest import TestCase
 from piccolo.columns.choices import Choice
 from piccolo.columns.column_types import Integer, Varchar
 from piccolo.table import Table
+from tests.example_apps.music.tables import Band, Manager
 
 
 class MyTable(Table):
@@ -111,4 +112,36 @@ class TestChoices(TestCase):
                 "mr": {"display_name": "Mr.", "value": 1},
                 "mrs": {"display_name": "Mrs.", "value": 2},
             },
+        )
+
+
+class TestEquals(TestCase):
+    def test_non_column(self):
+        """
+        Make sure non-column values don't match.
+        """
+        for value in (1, "abc", None):
+            self.assertFalse(Manager.name._equals(value))
+
+    def test_equals(self):
+        """
+        Test basic usage.
+        """
+        self.assertTrue(Manager.name._equals(Manager.name))
+
+    def test_same_name(self):
+        """
+        Make sure that columns with the same name, but on different tables,
+        don't match.
+        """
+        self.assertFalse(Manager.name._equals(Band.name))
+
+    def test_including_joins(self):
+        """
+        Make sure `including_joins` arg works correctly.
+        """
+        self.assertTrue(Band.manager.name._equals(Manager.name))
+
+        self.assertFalse(
+            Band.manager.name._equals(Manager.name, including_joins=True)
         )
