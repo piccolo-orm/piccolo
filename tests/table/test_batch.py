c3360f5bb155ec791eed65e08d4813ca79719a9a
Daniel Townsend <dan@dantownsend.co.uk>
2022-08-11 21:14:53 +0100

    different testing approach
    I changed this because we were modifying the global DB variable - this was we're just using a local DB variable.

--- tests/table/test_batch.py
+++ tests/table/test_batch.py
@@ -1,6 +1,13 @@
 import asyncio
 import math
+from unittest import TestCase
+from unittest.mock import MagicMock
 
+from piccolo.columns import Varchar
+from piccolo.engine.finder import engine_finder
+from piccolo.engine.postgres import AsyncBatch, PostgresEngine
+from piccolo.table import Table
+from piccolo.utils.sync import run_sync
 from tests.base import DBTestCase, postgres_only
 from tests.example_apps.music.tables import Manager
 
@@ -33,21 +40,6 @@ class TestBatchSelect(DBTestCase):
 
         return row_count, iterations
 
-    async def run_batch_extra_node(self, batch_size, node):
-        row_count = 0
-        iterations = 0
-
-        async with await Manager.select().batch(
-            batch_size=batch_size, node=node
-        ) as batch:
-            async for _batch in batch:
-                self._check_results(_batch)
-                _row_count = len(_batch)
-                row_count += _row_count
-                iterations += 1
-
-        return row_count, iterations
-
     def test_batch(self):
         row_count = 1000
         self.insert_many_rows(row_count)
@@ -56,29 +48,6 @@ class TestBatchSelect(DBTestCase):
 
         _row_count, iterations = asyncio.run(
             self.run_batch(batch_size=batch_size), debug=True
-        )
-
-        _iterations = math.ceil(row_count / batch_size)
-
-        self.assertEqual(_row_count, row_count)
-        self.assertEqual(iterations, _iterations)
-
-    def test_batch_extra_node(self):
-        from ..postgres_conf import DB
-
-        row_count = 1000
-        self.insert_many_rows(row_count)
-
-        # passing the test database as a read_replica to
-        # test if the batch method node argument works
-        DB.extra_nodes["read_replica"] = DB
-
-        batch_size = 10
-        node = "read_replica"
-
-        _row_count, iterations = asyncio.run(
-            self.run_batch_extra_node(batch_size=batch_size, node=node),
-            debug=True,
         )
 
         _iterations = math.ceil(row_count / batch_size)
@@ -127,3 +96,35 @@ class TestBatchObjects(DBTestCase):
 
         self.assertEqual(_row_count, row_count)
         self.assertEqual(iterations, _iterations)
+
+
+@postgres_only
+class TestBatchNodeArg(TestCase):
+    def test_batch_extra_node(self):
+        """
+        Make sure the batch methods can accept a node argument.
+        """
+
+        # Get the test database credentials:
+        test_engine = engine_finder()
+
+        EXTRA_NODE = MagicMock(spec=PostgresEngine(config=test_engine.config))
+
+        DB = PostgresEngine(
+            config=test_engine.config,
+            extra_nodes={"read_1": EXTRA_NODE},
+        )
+
+        class Manager(Table, db=DB):
+            name = Varchar()
+
+        # Testing `select`
+        response = run_sync(Manager.select().batch(node="read_1"))
+        self.assertIsInstance(response, AsyncBatch)
+        self.assertTrue(EXTRA_NODE.get_new_connection.called)
+        EXTRA_NODE.reset_mock()
+
+        # Testing `objects`
+        response = run_sync(Manager.objects().batch(node="read_1"))
+        self.assertIsInstance(response, AsyncBatch)
+        self.assertTrue(EXTRA_NODE.get_new_connection.called)
