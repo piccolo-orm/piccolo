5c6fb339d04d5829a4f2a99ab5fa120adf3504b6
Nathaniel Sabanski <sabanski.n@gmail.com>
2022-09-07 21:27:14 -0700

    To dict tests.

--- tests/table/instance/test_to_dict.py
+++ tests/table/instance/test_to_dict.py
@@ -1,6 +1,6 @@
 from tests.base import DBTestCase
 from tests.example_apps.music.tables import Band, Manager
-
+from tests.base import engine_is
 
 class TestToDict(DBTestCase):
     def test_to_dict(self):
@@ -11,7 +11,10 @@ class TestToDict(DBTestCase):
 
         instance = Manager.objects().first().run_sync()
         dictionary = instance.to_dict()
-        self.assertDictEqual(dictionary, {"id": 1, "name": "Guido"})
+        if engine_is('cockroach'):
+            self.assertDictEqual(dictionary, {"id": dictionary["id"], "name": "Guido"})
+        else:
+            self.assertDictEqual(dictionary, {"id": 1, "name": "Guido"})
 
     def test_nested(self):
         """
@@ -22,15 +25,26 @@ class TestToDict(DBTestCase):
 
         instance = Band.objects(Band.manager).first().run_sync()
         dictionary = instance.to_dict()
-        self.assertDictEqual(
-            dictionary,
-            {
-                "id": 1,
-                "name": "Pythonistas",
-                "manager": {"id": 1, "name": "Guido"},
-                "popularity": 1000,
-            },
-        )
+        if engine_is('cockroach'):
+            self.assertDictEqual(
+                dictionary,
+                {
+                    "id": dictionary["id"],
+                    "name": "Pythonistas",
+                    "manager": {"id": instance["manager"]["id"], "name": "Guido"},
+                    "popularity": 1000,
+                },
+            )
+        else:
+            self.assertDictEqual(
+                dictionary,
+                {
+                    "id": 1,
+                    "name": "Pythonistas",
+                    "manager": {"id": 1, "name": "Guido"},
+                    "popularity": 1000,
+                },
+            )
 
     def test_filter_rows(self):
         """
@@ -51,13 +65,22 @@ class TestToDict(DBTestCase):
 
         instance = Band.objects(Band.manager).first().run_sync()
         dictionary = instance.to_dict(Band.name, Band.manager.id)
-        self.assertDictEqual(
-            dictionary,
-            {
-                "name": "Pythonistas",
-                "manager": {"id": 1},
-            },
-        )
+        if engine_is('cockroach'):
+            self.assertDictEqual(
+                dictionary,
+                {
+                    "name": "Pythonistas",
+                    "manager": {"id": dictionary["manager"]["id"]},
+                },
+            )
+        else:
+            self.assertDictEqual(
+                dictionary,
+                {
+                    "name": "Pythonistas",
+                    "manager": {"id": 1},
+                },
+            )
 
     def test_aliases(self):
         """
@@ -69,4 +92,7 @@ class TestToDict(DBTestCase):
         dictionary = instance.to_dict(
             Manager.id, Manager.name.as_alias("title")
         )
-        self.assertDictEqual(dictionary, {"id": 1, "title": "Guido"})
+        if engine_is('cockroach'):
+            self.assertDictEqual(dictionary, {"id": dictionary["id"], "title": "Guido"})
+        else:
+           self.assertDictEqual(dictionary, {"id": 1, "title": "Guido"})
