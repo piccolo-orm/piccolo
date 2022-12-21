2b87cfa386169f152cabce7ed7497a431c53620f
Daniel Townsend <dan@dantownsend.co.uk>
2022-12-21 12:43:03 +0000

    fixtures insert rows in batches

--- piccolo/utils/list.py
+++ piccolo/utils/list.py
@@ -25,3 +25,37 @@ def flatten(
             _items.append(item)
 
     return _items
+
+
+def batch(
+    data: t.List[ElementType], chunk_size: int
+) -> t.List[t.List[ElementType]]:
+    """
+    Breaks the list down into sublists of the given ``chunk_size``. The last
+    sublist may have fewer elements than ``chunk_size``::
+
+        >>> batch(['a', 'b', 'c'], 2)
+        [['a', 'b'], ['c']]
+
+    :param data:
+        The data to break up into sublists.
+    :param chunk_size:
+        How large each sublist should be.
+
+    """
+    # TODO: Replace with itertools.batch when available:
+    # https://docs.python.org/3.12/library/itertools.html#itertools.batched
+
+    if chunk_size <= 0:
+        raise ValueError("`chunk_size` must be greater than 0.")
+
+    row_count = len(data)
+
+    iterations = int(row_count / chunk_size)
+    if row_count % chunk_size > 0:
+        iterations += 1
+
+    return [
+        data[(i * chunk_size) : ((i + 1) * chunk_size)]  # noqa: E203
+        for i in range(0, iterations)
+    ]
