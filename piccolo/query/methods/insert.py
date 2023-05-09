40c8e5b700e82bbe02e67e17a0242370a0765e4a
Daniel Townsend <dan@dantownsend.co.uk>
2023-05-09 17:40:14 +0100

    fix black error

--- piccolo/query/methods/insert.py
+++ piccolo/query/methods/insert.py
@@ -100,7 +100,7 @@ class Insert(
 
     @property
     def default_querystrings(self) -> t.Sequence[QueryString]:
-        base = f'INSERT INTO {self.table._meta.get_formatted_tablename()}'
+        base = f"INSERT INTO {self.table._meta.get_formatted_tablename()}"
         columns = ",".join(
             f'"{i._meta.db_column_name}"' for i in self.table._meta.columns
         )
