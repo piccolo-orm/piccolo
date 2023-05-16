142fc2e27fe8c54c31c3e6ab159f9b2a054062b5
Manh Luong <luongquangmanh85@gmail.com>
2023-05-16 18:34:28 +0700

    Add Count.distinct

--- piccolo/query/methods/count.py
+++ piccolo/query/methods/count.py
@@ -2,10 +2,11 @@ from __future__ import annotations
 
 import typing as t
 
+from piccolo.columns import Column
 from piccolo.custom_types import Combinable
 from piccolo.query.base import Query
 from piccolo.query.methods.select import Select
-from piccolo.query.mixins import WhereDelegate
+from piccolo.query.mixins import DistinctDelegate, WhereDelegate
 from piccolo.querystring import QueryString
 
 if t.TYPE_CHECKING:  # pragma: no cover
@@ -13,17 +14,30 @@ if t.TYPE_CHECKING:  # pragma: no cover
 
 
 class Count(Query):
-    __slots__ = ("where_delegate",)
+    __slots__ = (
+        "where_delegate",
+        "distinct_delegate",
+    )
 
     def __init__(self, table: t.Type[Table], **kwargs):
         super().__init__(table, **kwargs)
         self.where_delegate = WhereDelegate()
+        self.distinct_delegate = DistinctDelegate()
 
     ###########################################################################
     # Clauses
 
     def where(self: Self, *where: Combinable) -> Self:
         self.where_delegate.where(*where)
+        return self
+
+    def distinct(
+        self: Self, *, on: t.Optional[t.Sequence[Column]] = None
+    ) -> Self:
+        if on is not None and self.engine_type == "sqlite":
+            raise NotImplementedError("SQLite doesn't support DISTINCT ON")
+
+        self.distinct_delegate.distinct(enabled=True, on=on)
         return self
 
     ###########################################################################
@@ -35,6 +49,7 @@ class Count(Query):
     def default_querystrings(self) -> t.Sequence[QueryString]:
         select = Select(self.table)
         select.where_delegate._where = self.where_delegate._where
+        select.distinct_delegate._distinct = self.distinct_delegate._distinct
         return [
             QueryString(
                 'SELECT COUNT(*) AS "count" FROM ({}) AS "subquery"',
