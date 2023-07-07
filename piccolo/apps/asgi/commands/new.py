a70a90c984e6bdda3445f98013a4762412b3d694
Daniel Townsend <dan@dantownsend.co.uk>
2023-07-07 22:32:36 +0100

    pin fastapi version in new.py

--- piccolo/apps/asgi/commands/new.py
+++ piccolo/apps/asgi/commands/new.py
@@ -11,6 +11,9 @@ from jinja2 import Environment, FileSystemLoader
 TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), "templates/app/")
 SERVERS = ["uvicorn", "Hypercorn"]
 ROUTERS = ["starlette", "fastapi", "blacksheep", "litestar"]
+ROUTER_DEPENDENCIES = {
+    "fastapi": ["fastapi<0.100.0"],
+}
 
 
 def print_instruction(message: str):
@@ -51,7 +54,7 @@ def new(root: str = ".", name: str = "piccolo_project"):
 
     template_context = {
         "router": router,
-        "router_dependencies": [router],
+        "router_dependencies": ROUTER_DEPENDENCIES.get(router) or [router],
         "server": get_server(),
         "project_identifier": name.replace(" ", "_").lower(),
     }
