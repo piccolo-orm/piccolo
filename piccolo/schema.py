120518f66ee5ada754ce7ac9774d6461d191c3a8
Daniel Townsend <dan@dantownsend.co.uk>
2023-12-23 14:03:07 +0000

    fix errors in `schema.py`

--- piccolo/schema.py
+++ piccolo/schema.py
@@ -130,16 +130,19 @@ class ListTables:
         self.db = db
         self.schema_name = schema_name
 
-    async def run(self):
-        response = await self.db.run_querystring(
-            QueryString(
-                """
+    async def run(self) -> t.List[str]:
+        response = t.cast(
+            t.List[t.Dict],
+            await self.db.run_querystring(
+                QueryString(
+                    """
                 SELECT table_name
                 FROM information_schema.tables
                 WHERE table_schema = {}
                 """,
-                self.schema_name,
-            )
+                    self.schema_name,
+                )
+            ),
         )
         return [i["table_name"] for i in response]
 
@@ -154,9 +157,14 @@ class ListSchemas:
     def __init__(self, db: Engine):
         self.db = db
 
-    async def run(self):
-        response = await self.db.run_querystring(
-            QueryString("SELECT schema_name FROM information_schema.schemata")
+    async def run(self) -> t.List[str]:
+        response = t.cast(
+            t.List[t.Dict],
+            await self.db.run_querystring(
+                QueryString(
+                    "SELECT schema_name FROM information_schema.schemata"
+                )
+            ),
         )
         return [i["schema_name"] for i in response]
 
