c0f0ba3d75a3b0131f47977ba752ffd7becf3eb0
Daniel Townsend <dan@dantownsend.co.uk>
2024-01-22 22:30:40 +0000

    add a limit

--- piccolo/apps/user/commands/list.py
+++ piccolo/apps/user/commands/list.py
@@ -2,18 +2,23 @@ from piccolo.apps.user.tables import BaseUser
 from piccolo.utils.printing import print_dict_table
 
 
-def list_users():
+def list_users(limit: int = 20):
     """
     List existing users.
+
+    :param limit:
+        The maximum number of users to list.
+
     """
     users = (
         BaseUser.select(*BaseUser.all_columns(exclude=[BaseUser.password]))
         .order_by(BaseUser.username)
+        .limit(limit)
         .run_sync()
     )
 
-    if len(users) < 1:
+    if len(users) == 0:
         print("No data")
         return
 
-    print_dict_table(users)
+    print_dict_table(users, header_separator=True)
