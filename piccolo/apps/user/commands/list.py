bade80fe55dfbf9950d17dec6cd447fa05c19a27
Daniel Townsend <dan@dantownsend.co.uk>
2024-01-22 22:45:26 +0000

    add a test for the `piccolo user list` command (#922)
    * add a test for the `piccolo user list` command
    
    * add a limit
    
    * minor tweaks to `print_dict_table`

--- piccolo/apps/user/commands/list.py
+++ piccolo/apps/user/commands/list.py
@@ -2,14 +2,23 @@ from piccolo.apps.user.tables import BaseUser
 from piccolo.utils.printing import print_dict_table
 
 
-def list():
+def list_users(limit: int = 20):
     """
     List existing users.
+
+    :param limit:
+        The maximum number of users to list.
+
     """
     users = (
-        BaseUser.select(BaseUser.all_columns(exclude=[BaseUser.password]))
+        BaseUser.select(*BaseUser.all_columns(exclude=[BaseUser.password]))
         .order_by(BaseUser.username)
+        .limit(limit)
         .run_sync()
     )
 
-    print_dict_table(users)
+    if len(users) == 0:
+        print("No data")
+        return
+
+    print_dict_table(users, header_separator=True)
--- piccolo/utils/printing.py
+++ piccolo/utils/printing.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-def get_fixed_length_string(string: str, length=20) -> str:
+def get_fixed_length_string(string: str, length: int = 20) -> str:
     """
     Add spacing to the end of the string so it's a fixed length, or truncate
     if it's too long.
@@ -24,31 +24,34 @@ def print_heading(string: str, width: int = 64) -> None:
 def print_dict_table(data: List[dict], header_separator: bool = False) -> None:
     """
     Prints out a list of dictionaries in tabular form.
-    Uses the first list element to extract the
-    column names and their order within the row.
+
+    Uses the first list element to extract the column names and their order
+    within the row.
+
     """
+    if len(data) == 0:
+        raise ValueError("The data must have at least one element.")
 
-    if len(data) < 1:
-        print("No data")
-        return
-
-    ref_order = [column for column in data[0]]
-    width = {column: len(str(column)) for column in ref_order}
+    column_names = data[0].keys()
+    widths = {column_name: len(column_name) for column_name in column_names}
 
     for item in data:
-        for column in ref_order:
-            if len(str(item[column])) > width[column]:
-                width[column] = len(str(item[column]))
+        for column in column_names:
+            width = len(str(item[column]))
+            if width > widths[column]:
+                widths[column] = width
 
-    format_string = " | ".join([f"{{:<{width[w]}}}" for w in ref_order])
+    format_string = " | ".join(f"{{:<{widths[w]}}}" for w in column_names)
 
-    print(format_string.format(*[str(w) for w in ref_order]))
+    print(format_string.format(*[str(w) for w in column_names]))
 
     if header_separator:
         format_string_sep = "-+-".join(
-            [f"{{:<{width[w]}}}" for w in ref_order]
+            [f"{{:<{widths[w]}}}" for w in column_names]
         )
-        print(format_string_sep.format(*["-" * width[w] for w in ref_order]))
+        print(
+            format_string_sep.format(*["-" * widths[w] for w in column_names])
+        )
 
     for item in data:
-        print(format_string.format(*[str(item[w]) for w in ref_order]))
+        print(format_string.format(*[str(item[w]) for w in column_names]))
