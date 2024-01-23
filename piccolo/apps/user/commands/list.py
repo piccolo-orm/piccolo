e7c8a9ac8dc6b382301fc0f8bb8b44e8a3c8ce01
Daniel Townsend <dan@dantownsend.co.uk>
2024-01-23 12:59:32 +0000

    add a `page` arg to `piccolo user list` command

--- piccolo/apps/user/commands/list.py
+++ piccolo/apps/user/commands/list.py
@@ -2,18 +2,27 @@ from piccolo.apps.user.tables import BaseUser
 from piccolo.utils.printing import print_dict_table
 
 
-def list_users(limit: int = 20):
+def list_users(limit: int = 20, page: int = 1):
     """
     List existing users.
 
     :param limit:
         The maximum number of users to list.
+    :param page:
+        Lets you paginate through the list of users.
 
     """
+    if page < 1:
+        raise ValueError("The page number must > 0.")
+
+    if limit < 1:
+        raise ValueError("The limit number must be > 0.")
+
     users = (
         BaseUser.select(*BaseUser.all_columns(exclude=[BaseUser.password]))
         .order_by(BaseUser.username)
         .limit(limit)
+        .offset(limit * (page - 1))
         .run_sync()
     )
 
