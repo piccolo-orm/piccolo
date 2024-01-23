fb1ae3ec9df30e645ab5a4648f74f1fdea0cff9e
Daniel Townsend <dan@dantownsend.co.uk>
2024-01-23 22:11:37 +0000

    add `order_by` argument to `piccolo user list` command

--- tests/apps/user/commands/test_list.py
+++ tests/apps/user/commands/test_list.py
@@ -1,8 +1,9 @@
 from unittest import TestCase
-from unittest.mock import MagicMock, patch
+from unittest.mock import AsyncMock, MagicMock, patch
 
 from piccolo.apps.user.commands.list import list_users
 from piccolo.apps.user.tables import BaseUser
+from piccolo.utils.sync import run_sync
 
 
 class TestList(TestCase):
@@ -22,7 +23,7 @@ class TestList(TestCase):
         """
         Make sure the user information is listed, excluding the password.
         """
-        list_users()
+        run_sync(list_users())
 
         output = "\n".join(i.args[0] for i in print_mock.call_args_list)
 
@@ -31,19 +32,66 @@ class TestList(TestCase):
         assert self.user.password not in output
 
 
-class TestListArgs(TestCase):
-    def test_limit(self):
+class TestLimit(TestCase):
+    def test_non_positive(self):
         """
         Make sure non-positive `limit` values are rejected.
         """
         for value in (0, -1):
             with self.assertRaises(ValueError):
-                list_users(page=value)
+                run_sync(list_users(page=value))
 
-    def test_page(self):
+
+class TestPage(TestCase):
+    def test_non_positive(self):
         """
         Make sure non-positive `page` values are rejected.
         """
         for value in (0, -1):
             with self.assertRaises(ValueError):
-                list_users(limit=value)
+                run_sync(list_users(limit=value))
+
+
+class TestOrder(TestCase):
+    @patch("piccolo.apps.user.commands.list.get_users")
+    def test_order(self, get_users: AsyncMock):
+        """
+        Make sure valid column names are accepted.
+        """
+        get_users.return_value = []
+        run_sync(list_users(order_by="email"))
+
+        self.assertDictEqual(
+            get_users.call_args.kwargs,
+            {
+                "order_by": BaseUser.email,
+                "ascending": True,
+                "limit": 20,
+                "page": 1,
+            },
+        )
+
+    @patch("piccolo.apps.user.commands.list.get_users")
+    def test_descending(self, get_users: AsyncMock):
+        """
+        Make sure a colume name prefixed with '-' works.
+        """
+        get_users.return_value = []
+        run_sync(list_users(order_by="-email"))
+
+        self.assertDictEqual(
+            get_users.call_args.kwargs,
+            {
+                "order_by": BaseUser.email,
+                "ascending": False,
+                "limit": 20,
+                "page": 1,
+            },
+        )
+
+    def test_unrecognised_column(self):
+        """
+        Make sure invalid column names are rejected.
+        """
+        with self.assertRaises(ValueError):
+            run_sync(list_users(order_by="abc123"))
