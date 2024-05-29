95b8c7db4b4b34593f885650de6d975149763673
Daniel Townsend <dan@dantownsend.co.uk>
2024-05-29 11:38:02 +0100

    add ltrim and rtrim functions

--- piccolo/query/functions/string.py
+++ piccolo/query/functions/string.py
@@ -9,4 +9,12 @@ class Lower(Function):
     function_name = "LOWER"
 
 
-__all__ = ("Upper", "Lower")
+class Ltrim(Function):
+    function_name = "LTRIM"
+
+
+class Rtrim(Function):
+    function_name = "RTRIM"
+
+
+__all__ = ("Upper", "Lower", "Ltrim", "Rtrim")
