a79b86763e495a5f9c1526de3770165a0f88590b
Daniel Townsend <dan@dantownsend.co.uk>
2024-06-10 21:47:37 +0100

    add tests

--- tests/query/test_querystring.py
+++ tests/query/test_querystring.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 from piccolo.querystring import QueryString
+from tests.base import postgres_only
 
 
 # TODO - add more extensive tests (increased nesting and argument count).
@@ -28,3 +29,66 @@ class TestQueryString(TestCase):
     def test_querystring_with_no_args(self):
         qs = QueryString("SELECT name FROM band")
         self.assertEqual(qs.compile_string(), ("SELECT name FROM band", []))
+
+
+@postgres_only
+class TestQueryStringOperators(TestCase):
+    """
+    Make sure basic operations can be used on ``QueryString``.
+    """
+
+    def test_add(self):
+        query = QueryString("SELECT price") + 1
+        self.assertIsInstance(query, QueryString)
+        self.assertEqual(
+            query.compile_string(),
+            ("SELECT price + $1", [1]),
+        )
+
+    def test_divide(self):
+        query = QueryString("SELECT price") / 1
+        self.assertIsInstance(query, QueryString)
+        self.assertEqual(
+            query.compile_string(),
+            ("SELECT price / $1", [1]),
+        )
+
+    def test_power(self):
+        query = QueryString("SELECT price") ** 2
+        self.assertIsInstance(query, QueryString)
+        self.assertEqual(
+            query.compile_string(),
+            ("SELECT price ^ $1", [2]),
+        )
+
+    def test_subtract(self):
+        query = QueryString("SELECT price") - 1
+        self.assertIsInstance(query, QueryString)
+        self.assertEqual(
+            query.compile_string(),
+            ("SELECT price - $1", [1]),
+        )
+
+    def test_modulus(self):
+        query = QueryString("SELECT price") % 1
+        self.assertIsInstance(query, QueryString)
+        self.assertEqual(
+            query.compile_string(),
+            ("SELECT price % $1", [1]),
+        )
+
+    def test_like(self):
+        query = QueryString("strip(name)").like("Python%")
+        self.assertIsInstance(query, QueryString)
+        self.assertEqual(
+            query.compile_string(),
+            ("strip(name) LIKE $1", ["Python%"]),
+        )
+
+    def test_ilike(self):
+        query = QueryString("strip(name)").ilike("Python%")
+        self.assertIsInstance(query, QueryString)
+        self.assertEqual(
+            query.compile_string(),
+            ("strip(name) ILIKE $1", ["Python%"]),
+        )
