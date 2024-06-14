5a30410373e153f1ad86fd13b413ed4461553388
Daniel Townsend <dan@dantownsend.co.uk>
2024-06-15 00:02:27 +0100

    reduce repetition

--- piccolo/query/functions/datetime.py
+++ piccolo/query/functions/datetime.py
@@ -133,6 +133,32 @@ def _get_engine_type(identifier: t.Union[Column, QueryString]) -> str:
         raise ValueError("Unable to determine the engine type")
 
 
+def _extract_component(
+    identifier: t.Union[Date, Time, Timestamp, Timestamptz, QueryString],
+    sqlite_format: str,
+    postgres_format: ExtractComponent,
+    alias: t.Optional[str],
+):
+    engine_type = _get_engine_type(identifier=identifier)
+
+    return Cast(
+        (
+            Strftime(
+                identifier=identifier,
+                datetime_format=sqlite_format,
+                alias=alias,
+            )
+            if engine_type == "sqlite"
+            else Extract(
+                identifier=identifier,
+                datetime_component=postgres_format,
+                alias=alias,
+            )
+        ),
+        Integer(),
+    )
+
+
 def Year(
     identifier: t.Union[Date, Timestamp, Timestamptz, QueryString],
     alias: t.Optional[str] = None,
@@ -140,20 +166,12 @@ def Year(
     """
     Extract the year as an integer.
     """
-    engine_type = _get_engine_type(identifier=identifier)
-
-    if engine_type == "sqlite":
-        return Cast(
-            Strftime(identifier=identifier, datetime_format="%Y", alias=alias),
-            Integer(),
-        )
-    else:
-        return Cast(
-            Extract(
-                identifier=identifier, datetime_component="year", alias=alias
-            ),
-            Integer(),
-        )
+    return _extract_component(
+        identifier=identifier,
+        sqlite_format="%Y",
+        postgres_format="year",
+        alias=alias,
+    )
 
 
 def Month(
@@ -163,17 +181,12 @@ def Month(
     """
     Extract the month as an integer.
     """
-    engine_type = _get_engine_type(identifier=identifier)
-
-    if engine_type == "sqlite":
-        return Cast(
-            Strftime(identifier=identifier, datetime_format="%m", alias=alias),
-            Integer(),
-        )
-    else:
-        return Extract(
-            identifier=identifier, datetime_component="month", alias=alias
-        )
+    return _extract_component(
+        identifier=identifier,
+        sqlite_format="%m",
+        postgres_format="month",
+        alias=alias,
+    )
 
 
 def Day(
@@ -183,23 +196,12 @@ def Day(
     """
     Extract the day as an integer.
     """
-    engine_type = _get_engine_type(identifier=identifier)
-
-    if engine_type == "sqlite":
-        return Cast(
-            Strftime(identifier=identifier, datetime_format="%d"),
-            Integer(),
-            alias=alias,
-        )
-    else:
-        return Cast(
-            Extract(
-                identifier=identifier,
-                datetime_component="day",
-            ),
-            Integer(),
-            alias=alias,
-        )
+    return _extract_component(
+        identifier=identifier,
+        sqlite_format="%d",
+        postgres_format="day",
+        alias=alias,
+    )
 
 
 def Hour(
@@ -209,18 +211,12 @@ def Hour(
     """
     Extract the hour as an integer.
     """
-    engine_type = _get_engine_type(identifier=identifier)
-
-    if engine_type == "sqlite":
-        return Cast(
-            Strftime(identifier=identifier, datetime_format="%h"),
-            Integer(),
-            alias=alias,
-        )
-    else:
-        return Extract(
-            identifier=identifier, datetime_component="hour", alias=alias
-        )
+    return _extract_component(
+        identifier=identifier,
+        sqlite_format="%h",
+        postgres_format="hour",
+        alias=alias,
+    )
 
 
 def Minute(
@@ -230,17 +226,12 @@ def Minute(
     """
     Extract the minute as an integer.
     """
-    engine_type = _get_engine_type(identifier=identifier)
-
-    if engine_type == "sqlite":
-        return Cast(
-            Strftime(identifier=identifier, datetime_format="%m", alias=alias),
-            Integer(),
-        )
-    else:
-        return Extract(
-            identifier=identifier, datetime_component="minute", alias=alias
-        )
+    return _extract_component(
+        identifier=identifier,
+        sqlite_format="%m",
+        postgres_format="minute",
+        alias=alias,
+    )
 
 
 def Second(
@@ -250,17 +241,12 @@ def Second(
     """
     Extract the second as an integer.
     """
-    engine_type = _get_engine_type(identifier=identifier)
-
-    if engine_type == "sqlite":
-        return Cast(
-            Strftime(identifier=identifier, datetime_format="%s", alias=alias),
-            Integer(),
-        )
-    else:
-        return Extract(
-            identifier=identifier, datetime_component="second", alias=alias
-        )
+    return _extract_component(
+        identifier=identifier,
+        sqlite_format="%s",
+        postgres_format="second",
+        alias=alias,
+    )
 
 
 __all__ = (
