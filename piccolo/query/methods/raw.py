f4ff1966bab1ba8f65242c9627c0bd05d0897cb8
Daniel Townsend <dan@dantownsend.co.uk>
2024-09-16 16:20:52 +0100

    add `batch` to `raw` queries

--- piccolo/query/methods/raw.py
+++ piccolo/query/methods/raw.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import typing as t
 
+from piccolo.engine.base import BaseBatch
 from piccolo.query.base import Query
 from piccolo.querystring import QueryString
 
@@ -20,6 +21,18 @@ class Raw(Query):
     ):
         super().__init__(table, **kwargs)
         self.querystring = querystring
+
+    async def batch(
+        self,
+        batch_size: t.Optional[int] = None,
+        node: t.Optional[str] = None,
+        **kwargs,
+    ) -> BaseBatch:
+        if batch_size:
+            kwargs.update(batch_size=batch_size)
+        if node:
+            kwargs.update(node=node)
+        return await self.table._meta.db.batch(self, **kwargs)
 
     @property
     def default_querystrings(self) -> t.Sequence[QueryString]:
