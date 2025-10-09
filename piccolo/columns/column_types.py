3b77e7728953c8ef96cc85af90d648db20f4c3e1
Ryan Varley <rvarley@magnite.com>
2025-10-09 12:41:42 +0100

    more similar to current style

--- piccolo/columns/column_types.py
+++ piccolo/columns/column_types.py
@@ -158,7 +158,8 @@ class MathDelegate:
                 raise ValueError(
                     "Adding values across joins isn't currently supported."
                 )
-            return QueryString(f"{column_name} {operator} {value._meta.db_column_name}")
+            other_column_name = value._meta.db_column_name
+            return QueryString(f"{column_name} {operator} {other_column_name}")
         elif isinstance(value, (int, float)):
             if reverse:
                 return QueryString(f"{{}} {operator} {column_name}", value)
