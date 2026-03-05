963a55d188e570965f304914294cf3cd073e1ed9
Daniel Townsend <dan@dantownsend.co.uk>
2026-03-05 22:49:56 +0000

    add docs

--- piccolo/query/functions/conditional.py
+++ piccolo/query/functions/conditional.py
@@ -17,6 +17,42 @@ class Coalesce(QueryString):
     ):
         """
         Returns the first non-null value.
+
+        Here's an example to try in the playground::
+
+            >>> await Album.select(Album.release_date)
+            [
+                {'release_date': datetime.date(2021, 1, 1)},
+                {'release_date': datetime.date(2025, 1, 1)},
+                {'release_date': datetime.date(2022, 2, 2)},
+                {'release_date': None}
+            ]
+
+        One of the values is null - we can specify a fallback value::
+
+            >>> from piccolo.functions.conditional import Coalesce
+            >>> await Album.select(
+            ...     Coalesce(Album.release_date, datetime.date(2050, 1, 1))
+            ... )
+            [
+                {'release_date': datetime.date(2021, 1, 1)},
+                {'release_date': datetime.date(2025, 1, 1)},
+                {'release_date': datetime.date(2022, 2, 2)},
+                {'release_date': datetime.date(2050, 1, 1)}
+            ]
+
+        Or us this abbreviated syntax::
+
+            >>> await Album.select(
+            ...     Album.release_date | datetime.date(2050, 1, 1)
+            ... )
+            [
+                {'release_date': datetime.date(2021, 1, 1)},
+                {'release_date': datetime.date(2025, 1, 1)},
+                {'release_date': datetime.date(2022, 2, 2)},
+                {'release_date': datetime.date(2050, 1, 1)}
+            ]
+
         """
         if len(args) < 2:
             raise ValueError("At least two values must be passed in.")
