02b8efae8d7067e31b94f83059298f4d95a3dc1c
Daniel Townsend <dan@dantownsend.co.uk>
2026-08-06 13:07:14 +0100

    fix isort

--- tests/columns/test_defaults.py
+++ tests/columns/test_defaults.py
@@ -26,10 +26,7 @@ from piccolo.columns.defaults.numeric import (
     InfinityDecimal,
     NegativeInfinityDecimal,
 )
-from piccolo.columns.defaults.real import (
-    InfinityFloat,
-    NegativeInfinityFloat,
-)
+from piccolo.columns.defaults.real import InfinityFloat, NegativeInfinityFloat
 from piccolo.columns.defaults.timestamp import TimestampCustom
 from piccolo.columns.defaults.timestamptz import TimestamptzCustom
 from piccolo.table import Table
